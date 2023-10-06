e24b21fd54349978146d231c53d69c2ec5c9f8b1
bech <gabriel.marie@ynov.com>
2023-10-06 14:20:54 +0200

    super cool

--- script.js
+++ script.js
@@ -4,21 +4,29 @@ const image = header.querySelector('.header-image');
 const container = document.querySelector('.container')
 const menu = document.querySelector('.menu')
 const burgerdiv = document.querySelector('#burgerdiv')
+const blurdiv = document.querySelector('#blurdiv')
 
 function handleScroll() {
     if (window.scrollY > 50) {
         header.classList.add('scrolled');
         image.style.maxWidth = '85px';
-        menu.style.maxWidth = '60px'
+        image.style.maxHeight = '85px';
+        menu.style.maxWidth = '60px';
+        burgerdiv.style.marginTop = '85px'
+        burgerdiv.style.backgroundColor = 'transparent'
     } else {
         header.classList.remove('scrolled');
         image.style.maxWidth = '125px';
+        image.style.maxHeight = '125px';
         menu.style.maxWidth = '75px'
+        burgerdiv.style.marginTop = '125px'
+        burgerdiv.style.backgroundColor = "rgba(0, 0, 0, 0.316)";
     }
 }
 
-function blurr() {
-    this.style.display = fl;
+function hidediv() {
+    document.querySelector('#blurdiv').style.display = 'none'
+    menu.click()
 }
 
 window.addEventListener('scroll', handleScroll);
@@ -26,7 +34,9 @@ window.addEventListener('scroll', handleScroll);
 menu.addEventListener('click', () => {
     if (burgerdiv.style.display == 'flex') {
         burgerdiv.style.display = 'none'
+        blurdiv.style.display = 'none'
     } else {
         burgerdiv.style.display = 'flex'
+        blurdiv.style.display = 'block'
     }
 });
