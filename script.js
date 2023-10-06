c29f2db75bd5e1b195d7c87b5e452865911c5aae
bech <gabriel.marie@ynov.com>
2023-10-06 10:44:12 +0200

    super cool

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ const header = document.querySelector('.transparent-header');
 // Sélectionnez l'image à l'intérieur de l'en-tête
 const image = header.querySelector('.header-image');
 
-// Fonction de gestion du défilement
+const container = document.querySelector('.container')
+
 function handleScroll() {
-    if (window.scrollY > 50) { // Vous pouvez ajuster cette valeur selon votre préférence
+    if (window.scrollY > 50) {
         header.classList.add('scrolled');
-        image.style.maxWidth = '85px'; // Ajustez la taille de l'image ici
+        image.style.maxWidth = '85px';
     } else {
         header.classList.remove('scrolled');
-        image.style.maxWidth = '125px'; // Rétablissez la taille de l'image à 100%
+        image.style.maxWidth = '125px';
     }
 }
 
-// Écoutez l'événement de défilement
 window.addEventListener('scroll', handleScroll);
